b79684ccf80477ea2927ce287473631e0eeda9ac
Andrew Brennwald & Evan Clough <student@epicodus.com>
2015-09-15 16:50:53 -0700

    Add jquery handling for movie selection and displaying of movie times and available age tickets.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ function Movie(title, newRelease, times, score, rating, director, actors) {
   this.rating = rating;
   this.director = director;
   this.actors = actors;
+
 };
 
 Movie.prototype.allActors = function() {
@@ -33,20 +34,113 @@ Ticket.prototype.price = function() {
   if (this.newRelease) {
     basePrice += newFee;
   };
-  
+
   if (this.threeD) {
     basePrice += threeDFee;
   };
-  
+
   if (this.age == "child") {
     basePrice -= childDiscount;
   } else if (this.age == "senior") {
     basePrice -= seniorDiscount;
   };
-  
+
   if (this.time < "15:00") {
     basePrice *= matineeDiscount;
   };
 
   return "$" + basePrice;
 };
+
+var seedMovies = function () {
+  var madMax = new Movie("Mad Max: Fury Road", false, ["10:30", "13:00", "15:30", "18:00", "20:30"], 8.3, "R", "George Miller", ["Tom Hardy", "Charlize Theron", "Nicholas Hoult"]);
+
+  var dudeWheresMyWeed = new Movie("Dude Where's My Weed", false, ["4:20", "10:27", "13:04", "15:39", "18:12", "20:31"], 2.2, "NC-17", "Prince Billy Price", ["Tom Brady", "World", "The Queen of Wales"]);
+
+  var fastAndFurious95 = new Movie("The Fast and The Furious: We Need to Buy a New Stereo From Radioshack", true, ["10:30", "13:00", "15:30", "18:00", "20:30"], 0.1, "G", "Tricky Dick", ["Lyndon Johnson", "Jacqueline Onassis", "Arlene Schnitzer"]);
+
+  var movies = [madMax, dudeWheresMyWeed, fastAndFurious95];
+  return movies;
+};
+
+$(document).ready(function() {
+  var movies = seedMovies();
+
+// Populate movie selection dropdown with movie titles from movies
+// array, and assign them an index value for later reference.
+
+  movies.forEach(function(movie, i) {
+    $('#movie-input').append(
+      "<option value='" + i + "'>" + movie.title + "</option>"
+    );
+  });
+
+// Populate input fields with values for first movie in the movies
+// array on site load.
+
+  movies[0].times.forEach(function(time) {
+    $('#times-input').append(
+      "<label for='time'>" + time + "</label>" +
+      "<input type='radio' name='time' value='" + time + "'>");
+  });
+
+  $('#age-input').append(
+    "<label for='age'>adult</label>" +
+    "<input type='radio' name='age' value='adult'>" +
+
+    "<label for='age'>senior</label>" +
+    "<input type='radio' name='age' value='senior'>"
+  );
+
+  if (movies[0].rating == "G" || movies[0].rating == "PG") {
+    $('#age-input').append(
+      "<label for='age'>child</label>" +
+      "<input type='radio' name='age' value='child'>"
+    );
+  }
+
+// Change site content based on what movie is selected.
+
+  $('#movie-input').change(function() {
+    var movieIndex = parseInt($('#movie-input').val());
+
+// Wipe current information from price and input divs.
+
+    $('#times-input').children().remove();
+    $('#age-input').children().remove();
+    $('#three-d-input').children().remove();
+    $('#price').children().remove();
+
+// Creates a radio button for each viewing time available.
+
+    movies[movieIndex].times.forEach(function(time) {
+      $('#times-input').append(
+        "<label for='time'>" + time + "</label>" +
+        "<input type='radio' name='time' value='" + time + "'>");
+    });
+
+// Creates a radio button for adult and senior tickets.
+
+    $('#age-input').append(
+      "<label for='age'>adult</label>" +
+      "<input type='radio' name='age' value='adult'>" +
+
+      "<label for='age'>senior</label>" +
+      "<input type='radio' name='age' value='senior'>"
+    );
+
+// Creates a radio button for a child ticket if the movie is rated
+// appropriately for children.
+
+    if (movies[movieIndex].rating == "G" || movies[movieIndex].rating == "PG") {
+      $('#age-input').append(
+        "<label for='age'>child</label>" +
+        "<input type='radio' name='age' value='child'>"
+      );
+    }
+  });
+
+  // $('form#ticket-form').submit(function(event){
+  //   event.preventDefault();
+  // });
+});
